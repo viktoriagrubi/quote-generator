1f7aeea18d7c8db8d86b7c3c4364efc0b63e14a8
viktoriagrubi <wiktoria.grubizna@op.pl>
2025-03-19 20:24:49 +0100

    20 quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,44 @@
 import "./index.css";
 import { useState, useEffect } from "react";
 
-function getRandomQuote(quotes) {
-  return quotes[Math.floor(Math.random() * quotes.length)];
-}
-
 export default function App() {
   const [quotes, setQuotes] = useState([]);
   const [quote, setQuote] = useState(null);
 
   useEffect(() => {
-    fetch("https://type.fit/api/quotes")
+    fetch("https://dummyjson.com/quotes?limit=20")
       .then((res) => res.json())
-      .then((json) => {
-        setQuotes(json);
-        setQuote(json[0]);
-      });
+      .then((json) => setQuotes(json.quotes));
   }, []);
 
-  function getNewQuote() {
-    setQuote(getRandomQuote(quotes));
+  function getQuoteById(id) {
+    fetch(`https://dummyjson.com/quotes/${id}`)
+      .then((res) => res.json())
+      .then((json) => setQuote(json));
   }
 
   return (
     <main>
-      <h1>Project 3: Quote Generator</h1>
-      <section>
-        <button onClick={getNewQuote}>New Quote</button>
-        <h3>
-          <span>“</span>
-          {quote?.text}
-        </h3>
-        <i>- {quote?.author}</i>
-      </section>
+      <h1>Quote List</h1>
+      {quote ? (
+        <section>
+          <h3>
+            <span>“</span>
+            {quote.quote}
+          </h3>
+          <i>- {quote.author}</i>
+        </section>
+      ) : (
+        <section>
+          <ul>
+            {quotes.map((q) => (
+              <li key={q.id} onClick={() => getQuoteById(q.id)}>
+                {q.quote} - <i>{q.author}</i>
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
     </main>
   );
 }
